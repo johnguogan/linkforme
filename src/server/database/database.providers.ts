9bf0024be534a091605b9bf163bf1bb0c8fbe4df
terrygreen0606 <terrygreen0606@outlook.com>
2022-02-12 15:44:11 +0900

    fix script and replace mongo cloud

--- src/server/database/database.providers.ts
+++ src/server/database/database.providers.ts
@@ -4,6 +4,6 @@ export const databaseProviders = [
   {
     provide: 'DATABASE_CONNECTION',
     useFactory: (): Promise<typeof mongoose> =>
-      mongoose.connect(process.env.NODE_ENV === 'production' ? 'mongodb://linkforme:asf23gsgwaeagfsaf@mongo:27017/' : 'mongodb://localhost:27017/linkforme'),
+      mongoose.connect(process.env.NODE_ENV === 'production' ? 'mongodb+srv://wangdu422:pass1997422@cluster0.mqhdp.mongodb.net/' : 'mongodb://localhost:27017/linkforme'),
   },
 ];
